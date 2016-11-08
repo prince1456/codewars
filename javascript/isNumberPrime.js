f89f31b5d787c03e269b542d4bfb711cbcaf5009
Ali Akbar Alizada <akbar.alizadeh5@gmail.com>
2016-11-07 21:13:47 -0800

    add best solution for number is prime?

--- javascript/isNumberPrime.js
+++ javascript/isNumberPrime.js
@@ -23,3 +23,12 @@ function isPrime(n) {
         return true;
     }
 }
+
+
+// or THis best answer from reggna
+
+
+function isPrime(num) {
+  for (var i = 2; i < num; i++) if (num % i == 0) return false;
+  return num >= 2;
+}
