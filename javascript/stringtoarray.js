15dd8e79d678ecd2275335e2b00f0bc1ed9335aa
Ali Akbar Alizada <akbar.alizadeh5@gmail.com>
2016-12-07 20:31:05 -0800

    solved sting to array kata::

---
+++ javascript/stringtoarray.js
@@ -0,0 +1,11 @@
+Write a function to split a string and convert it into an array of words.
+
+e.g.
+
+Write a function to split a string and convert it into an array of words.
+
+e.g.
+function stringToArray(string){
+  return string.split(' ');
+}
+string_to_array("Robin Singh") => ["Robin", "Singh"]("Robin Singh") => ["Robin", "Singh"]
