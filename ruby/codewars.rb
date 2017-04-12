fc62f086993fe2690131fb16b357c971d42fcdb0
Ali Akbar Alizada <akbar.alizadeh5@gmail.com>
2017-04-11 19:10:09 -0700

    solve the Is this a triangle?

--- ruby/codewars.rb
+++ ruby/codewars.rb
@@ -39,3 +39,31 @@ def all? array, &block
     return resualt
 
 end
+
+
+
+# Implement a method that accepts 3 integer values a, b, c. The method should return true if a triangle can be built with the sides of given length and false in any other case.
+#
+# (In this case, all triangles must have surface greater than 0 to be accepted).
+# My way
+def isTriangle(a,b,c)
+  if a > 0 && b > 0 && c > 0
+      array = [a,b,c]
+      array = array.sort
+      if array[0]+array[1] > array[2]
+        return true
+      else
+        return false
+      end
+  else
+      return false
+  end
+end
+
+
+# Whaatt -_0
+
+def isTriangle(a,b,c)
+   a, b, c = [a, b, c].sort
+   a + b > c
+end
