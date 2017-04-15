254fb5399d8d6eddbe246c961472c6427b1da4e5
Ali Akbar Alizada <akbar.alizadeh5@gmail.com>
2017-04-14 23:33:42 -0700

    solved string to Array

--- ruby/codewars.rb
+++ ruby/codewars.rb
@@ -181,3 +181,21 @@ end
 def repeat_str (n, s)
    s * n
 end
+
+
+
+# Convert a string to an array
+# Description:
+#
+# Write a function to split a string and convert it into an array of words.
+#
+# e.g.
+#
+# string_to_array("Robin Singh") => ["Robin", "Singh"]
+#
+# string_to_array("I love arrays they are my favorite") => ["I", "love", "arrays", "they", "are", "my", "favorite"]
+#
+# Note: in Javascript the function is written in camelCase as 'stringToArray'
+def string_to_array(string)
+  string.split(" ")
+end
