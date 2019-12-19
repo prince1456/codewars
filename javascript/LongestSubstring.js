a6d2895d59a2c5a15ea1e5cca1942ed7d0b29d51
Ali Alizada <ali.alizada@qileap.com>
2019-12-18 16:26:59 -0800

    Find the longest substring in alphabetical order

--- javascript/LongestSubstring.js
+++ javascript/LongestSubstring.js
@@ -10,32 +10,23 @@
 // If there are multiple solutions, return the one that appears first.
 
 function longest(str) {
-    if(str.length === 1) return str
-  const alphabets = [...Array(26)]
-    .map(_ => String.fromCharCode(i++), (i = 97))
-  let longString = ''
-  let subSting = ''
+  if (str.length === 1) return str;
+  const alphabets = [...Array(26)].map(_ => String.fromCharCode(i++), (i = 97));
+  let longString = "";
+  let subSting = "";
   for (let x = 0; x < str.length; x++) {
-      let char = str.charAt(x);
-      const nextChar = str.charAt(x + 1);
-      let charIndex = alphabets.findIndex(alphabet =>  alphabet === char)
-      let nextCharIndex = alphabets.findIndex(alphabet =>  alphabet === nextChar)
-      console.log(nextCharIndex, charIndex)
-
-    if(nextCharIndex >= charIndex){
-        console.log(subSting, longString)
-        // if(!subSting.length) {
-        //     subSting = subSting + char
-        // }
-        console.log(subSting, longString)
-        subSting = subSting + nextChar
+    let char = str.charAt(x);
+    const nextChar = str.charAt(x + 1);
+    let charIndex = alphabets.findIndex(alphabet => alphabet === char);
+    let nextCharIndex = alphabets.findIndex(alphabet => alphabet === nextChar);
+    if (nextCharIndex >= charIndex) {
+      subSting = subSting + nextChar;
     } else {
-        if(!subSting.length) {
-            subSting = subSting + char
-        }
-        console.log(subSting.length, longString)
-        longString = subSting.length > longString.length ? subSting : longString
-        subSting = ''
+      if (!subSting.length) {
+        subSting = subSting + char;
+      }
+      longString = subSting.length > longString.length ? subSting : longString;
+      subSting = "";
     }
   }
   return longString;
