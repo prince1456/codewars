666e2e24668628381ca0540f004feefcc0654331
Ali Alizada <akbar.alizadeh5@gmail.com>
2022-10-30 23:51:37 -0700

    add other solution to ExtractTheDomainNameFromUrl

--- javascript/ExtractTheDomainNameFromUrl.js
+++ javascript/ExtractTheDomainNameFromUrl.js
@@ -1,10 +1,24 @@
-function domainName(url){
-    let mainDomain = url.includes("www") ? url.split('.')[1] : url.split('.')[0]
-    return mainDomain.includes("http") ? mainDomain.split('//')[1] : mainDomain
-  }
+//first Solution
+function domainName(url) {
+  let mainDomain = url.includes("www") ? url.split(".")[1] : url.split(".")[0];
+  return mainDomain.includes("http") ? mainDomain.split("//")[1] : mainDomain;
+}
 
+// second solution
+function domainName2(url) {
+  return url
+    .replace("http://", "")
+    .replace("https://", "")
+    .replace("www.", "")
+    .split(".")[0];
+}
 
- domainName("http://google.com")
- domainName("http://google.co.jp")
- domainName("www.xakep.ru")
- domainName("https://youtube.com")
+// 3rd solution use regex
+
+function domainName(url){  
+  return url.replace(/.+\/\/|www.|\..+/g, '')
+}
+domainName("http://google.com");
+domainName("http://google.co.jp");
+domainName("www.xakep.ru");
+domainName("https://youtube.com");
